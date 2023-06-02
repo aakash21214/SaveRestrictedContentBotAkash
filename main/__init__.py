15078fc0e771d2170c4d60f15698a682474a3f33
aakash21214 <93417145+aakash21214@users.noreply.github.com>
2023-06-02 23:30:48 +0000

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = config("API_ID", "12916125", cast=int)
+API_HASH = config("API_HASH", "dfebf9cc52b859771cf8a1d447e751a5")
+BOT_TOKEN = config("BOT_TOKEN", "5923462655:AAGABmVO38Umc3c764Doh1u3mdpW5yv773w")
 SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+FORCESUB = config("FORCESUB",erensupportgroup)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
